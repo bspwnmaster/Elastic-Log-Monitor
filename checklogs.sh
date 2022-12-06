ca771fb023ec97405820a1bff18ed1fa7c8b76d6
Bryce S <47609167+bspwnmaster@users.noreply.github.com>
2022-12-06 11:28:16 -0800

    Update checklogs.sh

--- checklogs.sh
+++ checklogs.sh
@@ -18,11 +18,11 @@ doc_count=$(curl -XGET -k -s "https://X.X.X.X:9200/_cat/count" -H 'Content-Type:
   }
 }')
 doc_compare="$(echo $doc_count |  cut -d ' ' -f 3)"
-last_mail="$(find /root/lastemailtime.txt -type f -mtime +1)" #Last alert sent more than 1 day ago
+last_mail="$(find /root/lastemailtime.txt -type f -mmin +720)" #Find if last alert sent more than 12 hours ago
 
 if [ $doc_compare -eq 0 ] && [ $last_mail ]
 then
-    echo "Elasticsearch no logs in 1 hour." 2>&1 | mail -s "Elasticsearch Error" example@gmail[.]com
+    echo "Elasticsearch no logs in 1 hour." 2>&1 | mail -s "Elasticsearch Error" example@email[.]com
     touch /root/lastemailtime.txt
 else
     exit 0
